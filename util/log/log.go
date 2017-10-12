e20687e9d30612c3171fafab97860e1779d17b53
liuzheng712 <liuzheng712@gmail.com>
2017-10-12 10:58:34 +0800

    fix(log): update the log...

--- util/log/log.go
+++ util/log/log.go
@@ -11,27 +11,11 @@ func init() {
 func Initial() {
 	golog.Initial()
 }
-func Debug(selector string, format string, v ...interface{}) {
-	golog.Debug(selector, format, v)
-}
-func Info(selector string, format string, v ...interface{}) {
-	golog.Info(selector, format, v)
-}
-func Notice(selector string, format string, v ...interface{}) {
-	golog.Notice(selector, format, v)
-}
-func Warn(selector string, format string, v ...interface{}) {
-	golog.Warn(selector, format, v)
-}
-func Error(selector string, format string, v ...interface{}) {
-	golog.Error(selector, format, v)
-}
-func Critical(selector string, format string, v ...interface{}) {
-	golog.Critical(selector, format, v)
-}
-func Panic(selector string, format string, v ...interface{}) {
-	golog.Panic(selector, format, v)
-}
-func Fatal(selector string, format string, v ...interface{}) {
-	golog.Fatal(selector, format, v)
-}
+var Debug = golog.Debug
+var Info = golog.Info
+var Notice = golog.Notice
+var Warn = golog.Warn
+var Error = golog.Error
+var Critical = golog.Critical
+var Panic = golog.Panic
+var Fatal = golog.Fatal
