34a8692156adbd63934d6d7e3e7dd0d0aa290276
liuzheng712 <liuzheng712@gmail.com>
2017-10-12 10:41:34 +0800

    feat(client): update

---
+++ client/client.go
@@ -0,0 +1,61 @@
+package client
+
+import (
+	"golang.org/x/crypto/ssh"
+	"net"
+	"coco/api"
+	"fmt"
+	"coco/util/log"
+)
+
+type Client struct {
+	Host   api.Machine
+	Credit api.LoginCredit
+	Signer ssh.Signer
+	Config *ssh.ClientConfig
+
+	Client  *ssh.Client
+	Session *ssh.Session
+
+	SSHForward string // TODO: ssh forward internal
+	Proxy      string // TODO: ssh use proxy
+}
+
+// 建立连接到后端服务器
+func New(host api.Machine, credit api.LoginCredit) (client Client, err error) {
+	client = Client{
+		Host:   host,
+		Credit: credit,
+	}
+	client.Signer, err = ssh.ParsePrivateKey([]byte(credit.PrivateKey))
+	if err != nil {
+		log.Error("client", "unable to parse private key: %v", err)
+	}
+	client.Config = &ssh.ClientConfig{
+		User: credit.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(client.Signer),
+		},
+		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+			return nil
+		},
+	}
+	client.Client, err = ssh.Dial("tcp", fmt.Sprintf("%s:%d", host.Ip, host.Port), client.Config)
+	if err != nil {
+		panic("Failed to dial: " + err.Error())
+	}
+
+	// Each ClientConn can support multiple interactive sessions,
+	// represented by a Session.
+	client.Session, err = client.Client.NewSession()
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+	return
+}
+
+// 关闭
+func (c *Client) Close() {
+	c.Session.Close()
+	c.Client.Close()
+}
