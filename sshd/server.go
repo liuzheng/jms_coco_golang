b34ca86950d8758cdff07526a13f39225c350117
liuzheng712 <liuzheng712@gmail.com>
2017-10-24 08:05:16 +0800

    feat(sshd): menu key switch

--- sshd/server.go
+++ sshd/server.go
@@ -22,13 +22,13 @@ type Server struct {
 	// recognized.. Returning nil error indicates that the login was allowed,
 	// regardless of whether the user was recognized or not. To disallow a
 	// connection, return an error.
-	Auther func(ssh.ConnMetadata, ssh.PublicKey) (*User, errors.Error)
+	Auther      func(ssh.ConnMetadata, ssh.PublicKey) (*User, errors.Error)
 
 	// Setup takes a Session, the most important task being filling out the
 	// permitted remote hosts. Returning an error here will send the error to
 	// the user and terminate the connection. This is not as clean as denying
 	// the user in Auther, but can be used in case the denial was too dynamic.
-	Setup func(*Session) errors.Error
+	Setup       func(*Session) errors.Error
 
 	// Interactive is called to ask the user to select a host on the list of
 	// potential remote hosts. This is only called in the case wehre more than
@@ -42,7 +42,7 @@ type Server struct {
 	// terminate the connection, allowing it to be used as a last-minute
 	// bailout.
 	//Selected  func(*Session, string) error
-	sshConfig *ssh.ServerConfig
+	sshConfig   *ssh.ServerConfig
 	//API       *api.Server
 }
 
@@ -100,7 +100,7 @@ func (s *Server) HandleConn(c net.Conn) {
 		}
 
 		menu.Welcome()
-	loop:
+		loop:
 		for {
 			fmt.Fprint(conn, "\r\nOpt>")
 			var buf []byte
@@ -114,8 +114,8 @@ func (s *Server) HandleConn(c net.Conn) {
 					break loop
 				}
 				if n >= 0 {
-					switch b[0] {
-					case '\r':
+					switch {
+					case 0x0A == b[0] || 0x0D == b[0]: // 换行键 or 回车键
 						if len(buf) == 1 {
 							fmt.Fprint(conn, "\r\n")
 							switch strings.ToUpper(string(buf[0])) {
@@ -125,12 +125,12 @@ func (s *Server) HandleConn(c net.Conn) {
 							case "G":
 								// 输入 G/g 显示您有权限的主机组.
 								menu.GetHostGroup()
-								//case "E":
-								////  输入 E/e 批量执行命令.(未完成)
-								//case "U":
-								////  输入 U/u 批量上传文件.(未完成)
-								//case "D":
-								////  输入 D/d 批量下载文件.(未完成)
+							//case "E":
+							////  输入 E/e 批量执行命令.(未完成)
+							//case "U":
+							////  输入 U/u 批量上传文件.(未完成)
+							//case "D":
+							////  输入 D/d 批量下载文件.(未完成)
 							case "H":
 								//  输入 H/h 帮助.
 								menu.GetHelp()
@@ -151,38 +151,55 @@ func (s *Server) HandleConn(c net.Conn) {
 								//  输入 G/g + 组ID 显示该组下主机. 如: g1
 								menu.GetHostGroupList(string(buf[1:]))
 							default:
-								// 输入 ID 直接登录 或 输入部分 IP,主机名,备注 进行搜索登录(如果唯一).
+							// 输入 ID 直接登录 或 输入部分 IP,主机名,备注 进行搜索登录(如果唯一).
 
 							}
 						}
 						continue loop
-						//res, err := strconv.ParseInt(string(buf), 10, 64)
-						//if log.HandleErr("DefaultInteractive", err) {
-						//	fmt.Fprint(conn, "input not a valid integer. Please try again")
-						//	continue loop
-						//}
-						//if int(res) >= 2 || res < 0 {
-						//	fmt.Fprint(conn, "No such server. Please try again")
-						//	continue loop
-						//}
-						//return remotes[int(res)], nil
-					case 0x03:
+					//res, err := strconv.ParseInt(string(buf), 10, 64)
+					//if log.HandleErr("DefaultInteractive", err) {
+					//	fmt.Fprint(conn, "input not a valid integer. Please try again")
+					//	continue loop
+					//}
+					//if int(res) >= 2 || res < 0 {
+					//	fmt.Fprint(conn, "No such server. Please try again")
+					//	continue loop
+					//}
+					//return remotes[int(res)], nil
+					case 0x03 == b[0]: // ctrl-c
 						fmt.Fprint(conn, "^C")
 						continue loop
-					case 0x04:
+					case 0x04 == b[0]: // ctrl-d
 						fmt.Fprint(conn, "^D\r\nGoodbye\r\n")
 						break loop
-						//return api.Machine{}, errors.New("user terminated session")
-					case 0x7f:
+					//return api.Machine{}, errors.New("user terminated session")
+					case 0x7f == b[0] || 0x08 == b[0]: // delete or backspace
 						if l := len(buf); l > 0 {
-							buf = buf[:l-1]
+							buf = buf[:l - 1]
 							fmt.Fprint(conn, "\b \b")
 						}
 						continue
-					default:
+					case 0x20 <= b[0] && b[0] <= 0x7E:
 						fmt.Fprintf(conn, "%s", b)
+					case 0x17 == b[0]://  方向键(←)
+						log.Debug("Opt loop", "方向键(←)")
+					case 0x38 == b[0]://  方向键(↑)
+						log.Debug("Opt loop", "方向键(↑)")
+
+					case 0x39 == b[0]://  方向键(→)
+						log.Debug("Opt loop", "方向键(→)")
+					case 0x40 == b[0]://  方向键(↓)
+						log.Debug("Opt loop", "方向键(↓)")
+					default:
+						log.Debug("Opt loop", "don't know key %v", b)
+						//fmt.Fprint(conn, "\b \b")
+						fmt.Fprintf(conn, "%s", "")
+
+						continue
+					//fmt.Fprintf(conn, "%s", b)
 					}
 					buf = append(buf, b[0])
+					log.Debug("Opt loop", "buf %v", buf)
 				}
 			}
 		}
