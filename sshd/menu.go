162a2ed3479de2a1d220315dd513b7dae28ba9a4
liuzheng712 <liuzheng712@gmail.com>
2017-10-25 10:51:53 +0800

    feat(menu): update

--- sshd/menu.go
+++ sshd/menu.go
@@ -8,13 +8,15 @@ import (
 	"coco/util/log"
 	"golang.org/x/crypto/ssh/terminal"
 	"io"
+	"strconv"
 )
 
 type Menu struct {
 	Conn    rw
 	Session *Session
-	API     *api.Server
-	Term    *terminal.Terminal
+	api     *api.Server
+	term    *terminal.Terminal
+	gid     int
 }
 
 var (
@@ -37,8 +39,13 @@ func NewMenu(conn rw, session *Session) (menu Menu, err error) {
 	if !as.Login(session.User.Name) {
 		return Menu{}, errors.New("Login fail", 403)
 	}
-	menu = Menu{Conn: conn, Session: session, API: as, Term:terminal.NewTerminal(conn, "Opt> ")}
-	//menu.Term.SetSize(140, 40)
+	menu = Menu{
+		Conn:    conn,
+		Session: session,
+		api:     as,
+		term:    terminal.NewTerminal(conn, "Opt[0]> "),
+		gid:     0}
+	//menu.term.SetSize(140, 40)
 	return
 }
 
@@ -46,7 +53,7 @@ func NewMenu(conn rw, session *Session) (menu Menu, err error) {
 func (m *Menu) Manager() {
 	defer fmt.Fprint(m.Conn, "Goodbye\r\n")
 	for {
-		command, err := m.Term.ReadLine()
+		command, err := m.term.ReadLine()
 		if err == io.EOF {
 			return
 		} else if log.HandleErr("Menu Manager", err, "logout") {
@@ -57,16 +64,16 @@ func (m *Menu) Manager() {
 			switch strings.ToUpper(string(command[0])) {
 			case "P":
 				// 输入 P/p 显示您有权限的主机.
-				m.GetMachineList()
+				m.Search("")
 			case "G":
 				// 输入 G/g 显示您有权限的主机组.
-				m.GetHostGroup()
-			//case "E":
-			////  输入 E/e 批量执行命令.(未完成)
-			//case "U":
-			////  输入 U/u 批量上传文件.(未完成)
-			//case "D":
-			////  输入 D/d 批量下载文件.(未完成)
+				m.GetHostGroups()
+				//case "E":
+				////  输入 E/e 批量执行命令.(未完成)
+				//case "U":
+				////  输入 U/u 批量上传文件.(未完成)
+				//case "D":
+				////  输入 D/d 批量下载文件.(未完成)
 			case "H":
 				//  输入 H/h 帮助.
 				m.GetHelp()
@@ -83,13 +90,15 @@ func (m *Menu) Manager() {
 				m.Search(command[1:])
 			case "G":
 				//  输入 G/g + 组ID 显示该组下主机. 如: g1
-				m.GetHostGroupList(command[1:])
+				m.gid, _ = strconv.Atoi(command[1:])
+				m.term.SetPrompt("Opt[" + strconv.Itoa(m.gid) + "]> ")
+				m.GetHostGroupList()
 			case "E":
 				if "xit" == command[1:] {
 					return
 				}
 			default:
-			// 输入 ID 直接登录 或 输入部分 IP,主机名,备注 进行搜索登录(如果唯一).
+				// 输入 ID 直接登录 或 输入部分 IP,主机名,备注 进行搜索登录(如果唯一).
 			}
 		}
 	}
@@ -102,30 +111,20 @@ func (m *Menu) Welcome() {
 	m.GetHelp()
 }
 
-// 获取主机列表
-func (m *Menu) GetMachineList() {
-	remotes := []api.Machine{}
-	format := "[%-4d]\t%-16s\t%-5d\t%s\t%s\t%s\r\n"
-	fmt.Fprintf(m.Conn, "[%-4s]\t%-16s\t%-5s\t%s\t%s\t%s\r\n", "ID", "IP", "Port", "Hostname", "Username", "Comment")
-	for _, v := range m.Session.Machines {
-		for _, u := range v.Users {
-			fmt.Fprintf(m.Conn, format, v.Sid, v.Ip, v.Port, "hostname", u.Username, v.Remark)
-			remotes = append(remotes, api.Machine{
-				Ip:     v.Ip,
-				Port:   v.Port,
-				Name:   v.Name,
-				Sid:    v.Sid,
-				Remark: v.Remark,
-				Users:  []api.MachineUser{u},
-			})
-		}
+// 获取主机组列表
+func (m *Menu) GetHostGroups() (err error) {
+	MachineGroup, err := m.api.GetGroupList()
+	if log.HandleErr("GetHostGroup", err, "") {
+		return err
+	}
+	format := "[%-4d]\t%-16s\t%s\r\n"
+	fmt.Fprintf(m.Conn, "[%-4s]\t%-16s\t%s\r\n", "GID", "Name", "Comment")
+
+	for _, v := range MachineGroup {
+		fmt.Fprintf(m.Conn, format, v.Gid, v.Name, v.Remark)
 	}
 
-}
-
-// 获取主机组内主机列表
-func (m *Menu) GetHostGroup() {
-
+	return
 }
 
 // 帮助页
@@ -136,12 +135,42 @@ func (m *Menu) GetHelp() {
 }
 
 // 获取主机组列表
-func (m *Menu) GetHostGroupList(id string) {
-	log.Debug("Menu GetHostGroupList", "%v", id)
+func (m *Menu) GetHostGroupList() (err error) {
+	Machine, errs := m.api.GetList("", m.gid)
+	if log.HandleErr("GetHostGroupList", errs, "") {
+		return errs
+	}
+	m.printMachines(Machine)
 
+	return
 }
 
 // 搜索主机
-func (m *Menu) Search(q string) {
+func (m *Menu) Search(q string) (err error) {
 	log.Debug("Menu Search", "%v", q)
+	m.Session.Machines, err = m.api.GetList(q, m.gid)
+	if log.HandleErr("GetMachineList", err, "") {
+		return err
+	}
+	m.printMachines(m.Session.Machines)
+	return
+}
+
+func (m *Menu) printMachines(Machines []api.Machine) {
+	format := "[%-4d]\t%-16s\t%-5d\t%s\t%-14s\t%s\r\n"
+	fmt.Fprintf(m.Conn, "[%-4s]\t%-16s\t%-5s\t%s\t%-14s\t%s\r\n", "ID", "IP", "Port", "Hostname", "Username", "Comment")
+	for _, v := range Machines {
+		for _, u := range v.Users {
+			fmt.Fprintf(m.Conn, format, v.Sid, v.Ip, v.Port, v.Name, u.Username, v.Remark)
+			//remotes = append(remotes, api.Machine{
+			//	Ip:     v.Ip,
+			//	Port:   v.Port,
+			//	Name:   v.Name,
+			//	Sid:    v.Sid,
+			//	Remark: v.Remark,
+			//	Users:  []api.MachineUser{u},
+			//})
+		}
+	}
+	return
 }
